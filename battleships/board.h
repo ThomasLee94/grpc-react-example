608b7ee6dae0a8766ce9579e9ac6d6048c388a95
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-15 18:12:18 -0700

    [add] board class

---
+++ battleships/board.h
@@ -0,0 +1,14 @@
+#ifndef BOARD_H_
+#define BOARD_H_
+
+namespace board {
+
+class Board {
+    public:
+        Board(const int y_coordinate, x_coordinate); 
+
+        void placeShip(int y_coordinate, int x_coordinate)
+}
+} // namespace board
+
+#endif //BOARD_H_
