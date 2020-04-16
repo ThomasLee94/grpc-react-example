e5c616a9f93d977b81e208f523147fa4445b47bb
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-16 14:33:05 -0700

    [fix] format, this & public

--- battleships/board.cc
+++ battleships/board.cc
@@ -8,21 +8,32 @@
 namespace board {
 
 Board::Board(const int rows, const int columns) {
-    this->GRID = GRID[columns][rows];
+    grid_ = grid_[columns][rows];
 }
 
-void Board::placeShip(const int y_start, const int y_end, const int x_start, const int x_end) {
+void Board::placeShip(
+        const int y_start,
+        const int y_end,
+        const int x_start,
+        const int x_end) {
+    // ensuring given coordinates are within the bounds of the grid
+    if (y_start >= rows || y_end > rows || x_start >= columns || x_end > columns) {
+
+    }
+
+    // ensuring ship placements doesnt collide with a ship that has been placed
+
     // vertical ship - x values remain the same
     if (std::abs(y_start - y_end) > 1) {
         for (int i = y_start; i < y_end, i++) {
-            GRID[i][x_start] = 1
+            grid_[i][x_start] = 1
         }
     }
     
     // horizontal ship - y values remain the same
     if (std::abs(x_start - x_end) > 1) {
         for (int i = x_start; i < x_end, i++) {
-            GRID[y_start][i] = i
+            grid_[y_start][i] = i
         }
     }
 
--- battleships/board.h
+++ battleships/board.h
@@ -6,9 +6,11 @@ namespace board {
 class Board {
     public:
         Board(const int rows, const int columns);
-        double GRID[columns][rows]; 
-
         void placeShip(const int y_coordinate_max, const int x_coordinate_max)
+    
+    private:
+        double grid_[columns][rows]; 
+
 }
 
 } // namespace board
