57861d207f193c186ff4316654a8325fc39c98b0
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-16 15:21:18 -0700

    [fix] place ship now accounts for 2 players

--- battleships/board.cc
+++ battleships/board.cc
@@ -12,10 +12,9 @@ Board::Board(const int rows, const int columns) {
 }
 
 void Board::placeShip(
-        const int y_start,
-        const int y_end,
-        const int x_start,
-        const int x_end) {
+        const int y_start, const int y_end,
+        const int x_start,const int x_end,
+        const int player) {
     // ensuring given coordinates are within the bounds of the grid
     if (y_start >= rows || y_end > rows || x_start >= columns || x_end > columns) {
         throw "Ship is either placed out of bounds";
@@ -24,10 +23,17 @@ void Board::placeShip(
     // vertical ship - x values remain the same
     if (std::abs(y_start - y_end) > 1) {
         for (int i = y_start; i < y_end, i++) {
-            if (grid_[i][x_start] == 1) {
-                throw "Ship collides with another ship";
+            if (player == 1) {
+                if (grid_[i][x_start] == 1) {
+                    throw "Ship collides with another ship";
+                }
+                grid_[i][x_start] = 1
             }
-            grid_[i][x_start] = 1
+
+            if (grid_[i+rows][x_start] == 1) {
+                    throw "Ship collides with another ship";
+                }
+            grid_[i+rows][x_start] = 1
             
         }
     }
@@ -35,10 +41,17 @@ void Board::placeShip(
     // horizontal ship - y values remain the same
     if (std::abs(x_start - x_end) > 1) {
         for (int i = x_start; i < x_end, i++) {
-            if (grid_[y_start][i] == 1) {
-                throw "Ship collides with another ship";
-            }
-            grid_[y_start][i] = 1
+            if (player == 1) {
+                if (grid_[y_start][i] == 1) {
+                    throw "Ship collides with another ship";
+                }
+                grid_[y_start][i] = 1
+            } 
+
+            if (grid_[y_start+rows][i] == 1) {
+                    throw "Ship collides with another ship";
+                }
+            grid_[y_start+rows][i] = 1
         }
     }
 
