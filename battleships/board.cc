9300801561503ea9ee2d0ab26249e73603d468f7
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-16 14:37:57 -0700

    [fix] error message placement

--- battleships/board.cc
+++ battleships/board.cc
@@ -18,22 +18,27 @@ void Board::placeShip(
         const int x_end) {
     // ensuring given coordinates are within the bounds of the grid
     if (y_start >= rows || y_end > rows || x_start >= columns || x_end > columns) {
-        throw "Ship is either placed out of bounds or collides with another ship";
+        throw "Ship is either placed out of bounds";
     }
-
-    // ensuring ship placements doesnt collide with a ship that has been placed
 
     // vertical ship - x values remain the same
     if (std::abs(y_start - y_end) > 1) {
         for (int i = y_start; i < y_end, i++) {
+            if (grid_[i][x_start] == 1) {
+                throw "Ship collides with another ship";
+            }
             grid_[i][x_start] = 1
+            
         }
     }
     
     // horizontal ship - y values remain the same
     if (std::abs(x_start - x_end) > 1) {
         for (int i = x_start; i < x_end, i++) {
-            grid_[y_start][i] = i
+            if (grid_[y_start][i] != 1) {
+                throw "Ship collides with another ship";
+            }
+            grid_[y_start][i] = 1
         }
     }
 
