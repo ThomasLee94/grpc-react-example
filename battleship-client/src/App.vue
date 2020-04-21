fed068c2d4ffa44e138895e2942f2e07aca34515
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-20 22:45:48 -0700

    [add] grid component

--- battleship-client/src/App.vue
+++ battleship-client/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Grid v-bind:grid="grid" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Grid from "./components/Grid";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  data() {
+    return {
+      grid: []
+    };
   }
-}
+};
 </script>
 
 <style>
---
+++ battleship-client/src/components/Grid.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div v-bind:key="cell.key" v-for="cell in grid">
+      <h2>{{cell.title}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Grid",
+  props: ["grid"]
+};
+</script>
+
+<style scoped>
+</style>
