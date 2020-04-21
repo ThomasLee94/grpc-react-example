f9de8e53c890483e5c885a5053eed9bc8daf359f
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-20 23:30:17 -0700

    [WIP] display a grid

---
+++ battleship-client/src/components/Cells.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h2>"I am a cell"</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cells",
+  props: ["cell"],
+  data() {
+    return {
+      grid: [
+        {
+          id: 1,
+          cellNum: 1
+        },
+        {
+          id: 2,
+          cellNum: 2
+        },
+        {
+          id: 3,
+          cellNum: 3
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
