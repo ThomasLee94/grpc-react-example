7ed6dacccd33a1a4e85cb171819689cdd4282a97
Thomas <thomaslee9474@gmail.com>
2020-04-24 13:13:36 -0700

    [add] grpc cli install script

---
+++ src/util/install_grpc_cli.sh
@@ -0,0 +1,12 @@
+sudo apt-get -y install build-essential autoconf libtool pkg-config
+sudo apt-get -y install libgflags-dev
+if ! [[ -f ~/bin/grpc_cli ]]; then
+  pushd "$(mktemp -d)"
+  git clone https://github.com/grpc/grpc.git ./
+  git submodule update --init
+  make grpc_cli
+  mv ./bins/opt/grpc_cli ~/bin/grpc_cli
+  popd
+fi
+
+# add the following to your path: export PATH="$PATH:$HOME/bin"
