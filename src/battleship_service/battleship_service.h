6504192aa1cd6b1b32b44465227b120aee6d44a6
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-23 18:03:16 -0700

    [add] battle ship service

--- src/battleship_service/battleship_service.h
+++ src/battleship_service/battleship_service.h
@@ -1,1 +1,18 @@
-// implemention of  
+#ifndef SRC_BATTLESHIP_UNARY_SERVICE_IMPL_H
+#define SRC_BATTLESHIP_UNARY_SERVICE_IMPL_H
+
+#include "grpc++/grpc++.h"
+
+#include "src/battleshipservice/proto/services.grpc.pb.h"
+
+namespace battleshipservice {
+
+class UnaryServiceImpl final : public ::battleshipservice::UnaryService::Service {
+ public:
+  grpc::Status BattleShip(
+      grpc::ServerContext* context,
+      const ::battleshipservice::BattleShipRequest* request,
+      ::battleshipservice::BattleShipResponse* response) override;
+};
+
+}  // namespace battleshipservice
