b55d30b16a3f12f5edab411f7fbca5fc09b11b43
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-26 14:43:49 -0700

    [fix] grpc web install script

--- src/helloworld/install_grpc_js_deps.sh
+++ src/helloworld/install_grpc_js_deps.sh
@@ -1,31 +1,20 @@
-# ------------------------ Clean Existing Generated Files ----------------------
-
-echo 'Cleaning existing generated files.'
-
-find generated -type f -name '*_pb.js' -delete
-find generated -type d -empty -delete
-mkdir -p generated
+FLAGS_protoc_version="3.10."
+FLAGS_grpc_web_plugin_version="1.0.6"
 
 # ------------------------------ Download protoc -------------------------------
+echo 'Downloading protoc.'
 
-# echo 'Downloading protoc.'
+PROTOC_URL="https://github.com/protocolbuffers/protobuf/releases/download/v${FLAGS_protoc_version:?}/protoc-${FLAGS_protoc_version:?}-linux-x86_64.zip"
+wget "$PROTOC_URL" -O protoc.zip -q --show-progress
 
-# PROTOC_URL="https://github.com/protocolbuffers/protobuf/releases/download/"
-# PROTOC_URL+="v${FLAGS_protoc_version:?}/protoc-${FLAGS_protoc_version:?}"
-# PROTOC_URL+="-linux-x86_64.zip"
-# wget "$PROTOC_URL" -O protoc.zip -q --show-progress
+unzip -q protoc.zip
 
-# unzip -q protoc.zip
-
-# echo 'protoc installed!'
+echo 'protoc installed!'
 
 # ------------------------ Download protoc-gen-grpc-web ------------------------
-
 echo 'Downloading protoc-gen-grpc-web.'
 
-PROTOC_GRPC_WEB_URL="https://github.com/grpc/grpc-web/releases/download/"
-PROTOC_GRPC_WEB_URL+="${FLAGS_grpc_web_plugin_version:?}/protoc-gen-grpc-web-"
-PROTOC_GRPC_WEB_URL+="${FLAGS_grpc_web_plugin_version:?}-linux-x86_64"
+PROTOC_GRPC_WEB_URL="https://github.com/grpc/grpc-web/releases/download/${FLAGS_grpc_web_plugin_version:?}/protoc-gen-grpc-web-${FLAGS_grpc_web_plugin_version:?}-linux-x86_64"
 wget "$PROTOC_GRPC_WEB_URL" -O protoc-gen-grpc-web -q --show-progress
 
 chmod +x protoc-gen-grpc-web
