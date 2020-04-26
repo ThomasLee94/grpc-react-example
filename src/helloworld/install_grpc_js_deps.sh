6c630d5b5486b09feeb82ae7d757dbffcd50cace
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-26 15:20:59 -0700

    [fix] script, again

--- src/helloworld/install_grpc_js_deps.sh
+++ src/helloworld/install_grpc_js_deps.sh
@@ -1,17 +1,22 @@
-FLAGS_protoc_version="3.10.0"
+FLAGS_protoc_version="3.10."
 FLAGS_grpc_web_plugin_version="1.0.6"
+readonly TMP_DIR="$(mktemp -d)"
 
 # ------------------------------ Download protoc -------------------------------
-echo 'Downloading protoc.'
 
-PROTOC_URL="https://github.com/protocolbuffers/protobuf/releases/download/v${FLAGS_protoc_version:?}/protoc-${FLAGS_protoc_version:?}-linux-x86_64.zip"
-wget "$PROTOC_URL" -O protoc.zip -q --show-progress
+# echo 'Downloading protoc.'
 
-unzip -q protoc.zip
+# PROTOC_URL="https://github.com/protocolbuffers/protobuf/releases/download/v${FLAGS_protoc_version:?}/protoc-${FLAGS_protoc_version:?}-linux-x86_64.zip"
+# wget "$PROTOC_URL" -O protoc.zip -q --show-progress
 
-echo 'protoc installed!'
+# unzip -q protoc.zip
+
+# mv bin/protoc "$TMP_DIR/protoc"
+
+# echo 'protoc installed!'
 
 # ------------------------ Download protoc-gen-grpc-web ------------------------
+
 echo 'Downloading protoc-gen-grpc-web.'
 
 PROTOC_GRPC_WEB_URL="https://github.com/grpc/grpc-web/releases/download/${FLAGS_grpc_web_plugin_version:?}/protoc-gen-grpc-web-${FLAGS_grpc_web_plugin_version:?}-linux-x86_64"
