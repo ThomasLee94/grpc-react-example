1542428fdca93b41abc308f520b2350229ecd8cb
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-27 14:36:44 -0700

    [fix] app.js

--- src/grpc-web-examples/grpc/src/App.js
+++ src/grpc-web-examples/grpc/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { googleRPC } = require("reactRPC")
+​
+const requests = require("helloworld_pb.js")
+​
+const clients = require("helloworld_grpc_web_pb.js")
+​
+const URL = "http://" + window.location.hostname + ":8080"
+​
+googleRPC.build(messages, services, URL)
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const stream = this.props.Greeter.sayRepeatHello(
+      { name: "Josh", count: 5, msgType: "RepeatHelloRequest" },
+      {}
+    );
+    stream.onMessage(res => {
+      console.log(res.getMessage());
+    });
+    console.log(stream);
+  }
+
+  render() {
+    console.log("This is the props", this.props);
+    this.props.Health.check(
+      { service: "Greeter", msgType: "HealthCheckRequest" },
+      {},
+      (err, response) => {
+        console.log(response.getStatus());
+      }
+    );
+    return (
+      <div>
+        <h1>googleRPC</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default googleRPC.wrapper(App);
--- src/grpc-web-examples/grpc/src/components/component.jsx
+++
@@ -1,9 +0,0 @@
-const { googleRPC } = require("reactRPC")
-​
-const messages = require("../helloworld_pb.js")
-​
-const services = require("../helloworld_grpc_web_pb.js")
-​
-const URL = "http://" + window.location.hostname + ":8080"
-​
-googleRPC.build(messages, services, URL)
