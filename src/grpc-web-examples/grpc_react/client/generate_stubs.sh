4109b2eca4964e4e46b24b40e291fa33e273aff2
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-27 23:41:20 -0700

    [add] script to generate stubs

--- src/grpc-web-examples/grpc_react/client/generate_stubs.sh
+++ src/grpc-web-examples/grpc_react/client/generate_stubs.sh
@@ -1,6 +1,6 @@
-$DIR="../server/proto"
-$OUT_DIR="/src/generated"
+DIR="./../server/proto"
+OUT_DIR="./generated/src"
 
-protoc -I=$DIR echo.proto --js_out=import_style=commonjs:$OUT_DIR
+protoc -I="$DIR" echo.proto --js_out=import_style=commonjs:$OUT_DIR
 
-protoc -I=$DIR echo.proto --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$OUT_DIR
+protoc -I="$DIR" echo.proto --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$OUT_DIR
