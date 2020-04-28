54662260341c84b97fa8c5191f7f53d4d15173b0
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-27 21:51:34 -0700

    [add] boilerplate for react example

---
+++ src/grpc-web-examples/grpc_react/server/echo_server.cc
@@ -0,0 +1,25 @@
+#include <grpcpp/grpcpp.h>
+#include <unistd.h>
+#include <string>
+
+#include "echo.grpc.pb.h"
+#include "echo_service_impl.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+
+void RunServer() {
+  std::string server_address("0.0.0.0:9090");
+  EchoServiceImpl service;
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  RunServer();
+
+  return 0;
+}
