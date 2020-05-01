62bba90c5c170a9c661f6d8430fce366e4deec2e
ThomasLee94 <thomaslee9474@gmail.com>
2020-04-30 17:09:56 -0700

    [rm] unecessary files

--- src/grpc-web-examples/grpc_react/server/generate_stub.sh
+++
@@ -1,7 +0,0 @@
-DIR="./proto"
-OUT_DIR="./generated"
-
-protoc -I="$DIR" --cpp_out="$OUT_DIR" echo.proto
-
-protoc -I="$DIR" --grpc_out="$OUT_DIR" --plugin=protoc-gen-grpc=grpc_cpp_plugin echo.proto
-# protoc -I="$DIR" --grpc_out="$OUT_DIR" --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` echo.proto
