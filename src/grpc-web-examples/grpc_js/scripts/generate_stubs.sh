5a41764f294255ee1cff7e6aa5fa4dda3f434f70
ThomasLee94 <thomaslee9474@gmail.com>
2020-05-02 22:48:57 -0700

    scripts work

--- src/grpc-web-examples/grpc_js/scripts/generate_stubs.sh
+++ src/grpc-web-examples/grpc_js/scripts/generate_stubs.sh
@@ -1,6 +1,6 @@
-DIR="./../server/proto"
-OUT_DIR="./src/generated/src"
+DIR="./server/proto"
+OUT_DIR="./generated"
 
-protoc-gen-grpc -I="$DIR" echo.proto --ts_out=$OUT_DIR
+protoc-gen-grpc -I="$DIR" echo.proto --js_out=$OUT_DIR
 
 protoc-gen-grpc -I="$DIR" echo.proto --grpc-web_out=mode=grpcwebtext:$OUT_DIR
