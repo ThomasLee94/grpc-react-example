6e9881c2bf7d595302bd4b514e34b7a184ed2f2a
ThomasLee94 <thomaslee9474@gmail.com>
2020-05-02 21:09:18 -0700

    [add] cpp/grpc server

---
+++ src/grpc-web-examples/grpc_js/server/echo_server.cc
@@ -0,0 +1,25 @@
+#include "grpc++/grpc++.h"
+#include <unistd.h>
+#include <string>
+
+#include "src/grpc-web-examples/grpc_react/server/proto/echo.grpc.pb.h"
+#include "echo_service_impl.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+
+void RunServer() {
+  std::string server_address("0.0.0.0:9090");
+  EchoServiceImpl service;
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  RunServer();
+
+  return 0;
+}
