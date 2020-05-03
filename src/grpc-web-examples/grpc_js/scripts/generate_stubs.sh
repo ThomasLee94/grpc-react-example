d4861d29d6b7f75632025e0ab11b90d6704b492c
ThomasLee94 <thomaslee9474@gmail.com>
2020-05-03 01:33:12 -0700

    old stub output

--- src/grpc-web-examples/grpc_js/scripts/generate_stubs.sh
+++ src/grpc-web-examples/grpc_js/scripts/generate_stubs.sh
@@ -1,6 +1,10 @@
 DIR="./server/proto"
 OUT_DIR="./generated"
 
-protoc-gen-grpc -I="$DIR" echo.proto --js_out=$OUT_DIR
+# protoc-gen-grpc -I="$DIR" echo.proto --js_out=$OUT_DIR
 
-protoc-gen-grpc -I="$DIR" echo.proto --grpc-web_out=mode=grpcwebtext:$OUT_DIR
+# protoc-gen-grpc -I="$DIR" echo.proto --grpc-web_out=mode=grpcwebtext:$OUT_DIR
+
+protoc -I="$DIR" echo.proto --js_out=import_style=commonjs:$OUT_DIR
+
+protoc -I="$DIR" echo.proto --grpc-web_out=import_style=commonjs,mode=grpcwebtext:$OUT_DIR
