1a1cbcf9721f52b1f99d447294211661e33edb39
ThomasLee94 <thomaslee9474@gmail.com>
2020-05-05 23:09:53 -0700

    [add] comment

--- server/run_envoy.sh
+++ server/run_envoy.sh
@@ -1,3 +1,6 @@
+# The linking of network is meant for dev purposes,
+# they would be linked with ports on kubernetes for deployment
+
 ROOT_DIR=$(git rev-parse --show-toplevel)
 pushd "$ROOT_DIR/server/envoy" 
 docker build -t tom/envoy .
